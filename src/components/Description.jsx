2a9c457e26d5f8de23252a9e29dc7f8ebec63cfd
elea641 <eleavoliotis64@gmail.com>
2023-03-29 14:05:04 +0200

    first commit

---
+++ src/components/Description.jsx
@@ -0,0 +1,9 @@
+function Description() {
+    const text = "Ici achetez toutes les plantes dont vous avez toujours rêvées"
+    const emojis = "🌵🌱🎍"
+    return (
+    <p>{ text.slice(0, 11) + emojis }</p>)
+}
+
+export default Description
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+function Header() {
+    const titleBanner = "La maison jungle"
+    return <h1>{titleBanner.toUpperCase()}</h1>
+}
+
+export default Header;
