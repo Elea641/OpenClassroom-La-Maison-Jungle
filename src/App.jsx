0c60d4d10f62fbd43db3d05fd4bff843b80f9754
elea641 <eleavoliotis64@gmail.com>
2023-03-30 13:51:44 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Banner from './components/Banner'
 import Cart from './components/Cart'
+import ShoppingList from './components/ShoppingList'
+import './App.css'
+
 
 function App() {
 	return (
 		<div>
 			<Banner />
 			<Cart />
+			<ShoppingList />
 		</div>
 	)
 }
 
 export default App
+
